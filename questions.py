8b8ce0ed72d21bf203b77ea489d13b21e232192e
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2022-10-08 00:33:37 -0700

    Moved most things into questions.py. Created a interface class. Also can display quesiton info now

--- questions.py
+++ questions.py
@@ -1,4 +1,15 @@
 import requests
+import tkinter
+from html import unescape
+import random
+
+# actual question
+class Question:
+    def __init__(self, question):
+        self.question = unescape(question['question'])
+        self.answer = unescape(question['correct_answer'])
+        self.type = unescape(question['type'])
+        self.wrong_answers = unescape(question['incorrect_answers'])
 
 # holds the questions
 class QuestionBank:
@@ -6,6 +17,7 @@ class QuestionBank:
         request = requests.get('https://opentdb.com/api.php?amount=10&type=multiple')
         results = request.json()['results']
         self.questions = []
+        self.question_num = 0
         for question in results:
             print(question)
             new_question = Question(question)
@@ -17,11 +29,43 @@ class QuestionBank:
             print(question.type)
             print(question.wrong_answers)
 
-class Question:
-    def __init__(self, question):
-        self.question = question['question']
-        self.answer = question['correct_answer']
-        self.type = question['type']
-        self.wrong_answers = question['incorrect_answers']
+    def get_current_question(self):
+        return self.questions[self.question_num]
+
+# quiz interface
+class Interface:
+    def __init__(self, questionsBank):
+        self.window = tkinter.Tk()
+        self.window.title("Trivia Game")
+        self.window.minsize(height=400, width=500)
+        self.window.resizable(False, False)
+
+        self.question_box = tkinter.Label(self.window, text=questionsBank.questions[0].question, background='red')
+        self.question_box.grid(columnspan=2)
+
+        self.button1 = tkinter.Button(self.window, text='Button1')
+        self.button2 = tkinter.Button(self.window, text='button2')
+        self.button3 = tkinter.Button(self.window, text='button3')
+        self.button4 = tkinter.Button(self.window, text='button4')
+        self.button1.grid(row=1, column=0, padx=20, pady=20)
+        self.button2.grid(row=1, column = 1, padx=20, pady=20)
+        self.button3.grid(row=2, column=0, padx=20, pady=20)
+        self.button4.grid(row=2, column=1, padx=20, pady=20)
+
+    def get_next_question(self, question_bank):
+        current_question = question_bank.get_current_question()
+        self.question_box.config(text=current_question.question)
+        l = [self.button1, self.button2, self.button3, self.button4]
+        # make the answer_button first
+        answer_button = l.pop(random.randrange(len(l)))
+        answer_button.config(text=current_question.answer)
+        # now assign the wrong answers to the other buttons
+        for i in range(len(l)):
+            l[i].config(text=current_question.wrong_answers[i])
+
 
 questionsBank = QuestionBank()
+interface = Interface(questionsBank)
+interface.get_next_question(questionsBank)
+
+interface.window.mainloop()
